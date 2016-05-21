69922009407f33d531d041034ee899fbd4cab1ca
Michiel Vandendriessche <michielvdd@me.com>
2016-05-21 19:00:54 +0200

    activity analyzer

---
+++ analyzer/analyze/accelerometerdata.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+from numpy import mean
+
+class AccelerometerData(object):
+
+    data = None
+    base = None
+    thresholds = None
+
+    def __init__(self, data):
+        self.base = mean(data)
+        self.thresholds = {
+            "low": 1000,
+            "medium": 3000,
+            "high": 5000
+        }
+        self.data = map(lambda x: abs(x-self.base), data)
+
+    def getScore(self):
+        #plt.figure()
+        #plt.plot(self.data)
+        bottomCount = len([el for el in self.data if el <= self.thresholds["low"]])
+        lowCount = len([el for el in self.data if self.thresholds["low"] < el <= self.thresholds["medium"]])
+        mediumCount = len([el for el in self.data if self.thresholds["medium"] < el <= self.thresholds["high"]])
+        highCount = len([el for el in self.data if self.thresholds["high"] < el])
+        bottomRatio = (float(bottomCount) / len(self.data))
+        lowRatio = (float(lowCount) / len(self.data))
+        mediumRatio = (float(mediumCount) / len(self.data))
+        highRatio = (float(highCount) / len(self.data))
+        ratio = bottomRatio + lowRatio + mediumRatio + highRatio
+        bottomRatio *= 3
+        lowRatio *= 5
+        mediumRatio *= 7
+        highRatio *= 10
+        ratio = lowRatio + mediumRatio + highRatio
+        #plt.title(ratio)
+        #plt.show()
+        return ratio
--- analyzer/analyze/activitydata.py
+++ analyzer/analyze/activitydata.py
@@ -1,23 +1,37 @@
 from data import Data
 import matplotlib.pyplot as plt
+from numpy import mean
+from accelerometerdata import AccelerometerData
 
 
 class ActivityData(Data):
 
     name = "activity"
+    minutes = 5
 
     def getAnalyzed(self):
         data = self.extractData()
-        for sensor, values in data.items():
-            if sensor == "accelerometer":
-                print sensor
-                print values
-                plt.plot(values)
-                plt.title(sensor)
-        plt.show()
+        #plt.figure()
+        #plt.plot(values)
+        valuesList = self.divideData(data, self.minutes)
+        scores = list()
+        for values in valuesList:
+            acc = AccelerometerData(values)
+            score = acc.getScore()
+            scores.append(score)
+        score = mean(scores)
+        minActive = len([el for el in scores if el > 5])
+        # plt.show()
+        return {
+            "score": score,
+            "minActive": minActive
+        }
 
     def extractData(self):
-        data = dict()
-        data["accelerometer"] = [el[0] for el in self.data["accelerometer"]]
-        data["steps"] = [el[0] for el in self.data["steps"]]
-        return data
+        return [el[0] for el in self.data["accelerometer"]]
+
+    def divideData(self, data, k):
+        l = len(data)
+        n = int(round(l/k))
+        result = [data[i:i+n] for i in xrange(0, l, n)]
+        return result
